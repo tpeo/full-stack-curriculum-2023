0f4b8b29b9e2f3dc36945a6256aa799fea0d5d4f
Vincent Do <vincentvietdo@gmail.com>
2023-11-02 08:52:57 -0500

    prepping demo!

--- Week_7/demo/index.js
+++ Week_7/demo/index.js
@@ -1,205 +1,135 @@
-const { randomInt } = require('crypto');
-// require express returns a function
-const express = require('express');
+// First run:
+// npm init --yes
+// npm install express
+// make sure to add a .gitignore for .env and node modules
 
+const { randomInt } = require("crypto");
+// require express returns a function
+const express = require("express");
 // returns an object
 const app = express();
-const db = require('./firebase')
-
-// for hashing
-var pbkdf2 = require('pbkdf2')
-// JWT 
-const jwt = require('jsonwebtoken');
-// store in env, ok here for demo
-ACCESS_TOKEN_SECRET = "abc123"
-const SALT = ";asf;klsadfllsfjalskdfjl";
-
+const db = require("./firebase");
 // for env variables
 // run npm install dotenv for dependency
-require('dotenv').config()
-
+require("dotenv").config();
 // to parse JSON in req, res
-app.use(express.json())
+app.use(express.json());
+
+
+// STEP ONE: Import required modules
+
+
+// STEP TWO: Store constants for access token secret and salt
+
+
+
+// STEP FIVE: Creating an auth middleware
+
+
 
 // Middleware to validate tweet length
 const validateTweetLength = (req, res, next) => {
-    const tweet = req.body.tweet;
-    if (tweet.length <= 100) {
-      next(); // Tweet is valid, proceed to the next middleware or route
-    } else {
-      res.status(400).json({ error: 'Tweet is too long (max 100 characters).' });
-    }
-  };
-
-  // Middleware to validate input of post request
-  const validateInput = (req, res, next) => {
-    const user = req.user;
-    const tweet = req.body.tweet;
-    if (!tweet || !user) {
-        res.status(400).json({ error: 'Incomplete input' });
-    } else {
-        next();
-    }
-  };
-
-  // Auth Middleware for non expiring tokens (check validity of token sent in)
-function authMiddleware(req, res, next) {
-  // Check if proper header exists
-  if (req.headers["authorization"]) {
-    // Split on space -> should return ["Bearer", "${token}"]
-    const headers = req.headers["authorization"].split(" ");
-    // Check if first argument is Bearer
-    if (headers.length === 2 && headers[0] === "Bearer") {
-      let token = headers[1];
-      try {
-        // verify the token
-        let decodedToken = jwt.verify(token, ACCESS_TOKEN_SECRET);
-        // Set user object which can be accessed in the req in future
-        req.user = decodedToken.username;
-        next(); // Go to next function
-      } catch (e) {
-        return res.status(401).json({ msg: e.message });
-      }
-    } else {
-      return res.status(401).json({ msg: "invalid token" });
-    }
+  const tweet = req.body.tweet;
+  if (tweet.length <= 100) {
+    next(); // Tweet is valid, proceed to the next middleware or route
   } else {
-    return res.status(401).json({ msg: "token was not found in header" });
+    res.status(400).json({ error: "Tweet is too long (max 100 characters)." });
   }
-}
-
-
-app.get('/', (req, res) => {
-    res.send('Hello World')
-})
-
-// get all tweets
-app.get('/api/tweets', authMiddleware, async (req, res) => {
-    const doc = await db.collection("tweets").doc("tweets").get();
-    res.send(doc.data().tweets)
-})
-
-// get tweets by user (param in route)
-app.get('/api/tweets/:user', authMiddleware, async (req, res) => {
-    const doc = await db.collection("tweets").doc("tweets").get();
-    const tweets = doc.data().tweets;
-    var target = tweets.find(t => t.user === req.user)
-    if (!target) {
-        res.status(404).send("Tweet not found")
-    } else {
-        res.send(target)
-    }
-})
-
-// post a tweet
-app.post('/api/tweets', authMiddleware, validateInput, validateTweetLength, async (req, res) => {
-    var tweet = {
-        id: randomInt(1000),
-        user: req.user,
-        tweet: req.body.tweet
-    }
-    const tweetsRef = db.collection("tweets").doc("tweets");
-    const snapshot = await tweetsRef.get();
-    const currTweets = snapshot.data().tweets;
-    currTweets.push(tweet)
-    await tweetsRef.update({tweets: currTweets})
-    res.send(tweet)
-})
-
-// delete a tweet
-app.delete('/api/tweets', authMiddleware, async (req, res) => {
-    const tweetsRef = db.collection("tweets").doc("tweets");
-    const snapshot = await tweetsRef.get();
-    const currTweets = snapshot.data().tweets;
-    const tweetIndex = currTweets.findIndex(tweet => tweet.id === req.body.id);
-    if (tweetIndex === -1) {
-        res.status(404).send("Tweet not found");
-      } else {
-        // Remove the tweet from the 'tweets' array
-        var removed = currTweets[tweetIndex]
-        console.log(removed)
-        currTweets.splice(tweetIndex, 1);
-        await tweetsRef.update({tweets: currTweets})
-        res.json(removed);
-      }
-})
-
-// NEW
-// Hash a password function using PBKDF2
-const hashPassword = (password) => {
-  const key = pbkdf2.pbkdf2Sync(password, SALT, 1000, 64, 'sha512');
-  return key.toString('hex');
 };
 
-// Creates a user with password, no checks needed
-app.post("/register", async (req, res) => {
-  // Get the username and password from request
-  const { username, password } = req.body;
-  // hash the password - this is what we want to store in db
-  const passHashed = hashPassword(password)
-  // Check for duplicate users
-  const check = await db.collection("users").doc(username).get();
-  if (check.exists) {
-    return res.status(400).json({ msg: "User exists" });
-  }
-  // create new user
-  const user = {
-    username: username,
-    password: passHashed
-  }
-console.log(user)
-// add new user to DB
-  const usersRef = db.collection('users');
-  await usersRef.doc(username).set(user);
-// create new access token
-// set expiry to 30s so you can show them how it expires when call route
-  const accessToken = jwt.sign(
-    { "username": username },
-    ACCESS_TOKEN_SECRET,
-    { expiresIn: '30s' }
-  );  
-  // Send JWT Token back
-  res.json({
-    msg: "successfully created",
-    data: { username: username },
-    token: accessToken,
-  });
-});
-
-// Verifies password + creates token
-app.post("/login", async (req, res) => {
-  const { username, password } = req.body;
-  const passHashed = hashPassword(password);
-  // Get the user
-  const check = await db.collection("users").doc(username).get();
-  // Check if user exists
-  if (!check.exists) {
-    return res.status(400).json({ msg: "User does not exist" });
-  }
-  // Cross reference the stored password with the incoming password (hashed)
-  const user = check.data();
-  let samePassword = passHashed === user.password;
-  if (samePassword) {
-    // user logged in correctly
-    const accessToken = jwt.sign(
-      { "username": username },
-      ACCESS_TOKEN_SECRET,
-      { expiresIn: '30s' }
-    );
-    return res.json({
-      msg: "successfully logged in",
-      data: { username: username },
-      token: accessToken,
-    });
+// Middleware to validate input of post request
+const validateInput = (req, res, next) => {
+  const user = req.body.user;
+  const tweet = req.body.tweet;
+  if (!tweet || !user) {
+    res.status(400).json({ error: "Incomplete input" });
   } else {
-    return res.status(401).json({ msg: "Username or password was incorrect" });
+    next();
+  }
+};
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+// get all tweets
+app.get("/api/tweets", async (req, res) => {
+  const doc = await db.collection("tweets").doc("tweets").get();
+  res.send(doc.data().tweets);
+});
+
+// get tweets by user (param in route)
+app.get("/api/tweets/:user", async (req, res) => {
+  const doc = await db.collection("tweets").doc("tweets").get();
+  const tweets = doc.data().tweets;
+  var target = tweets.find((t) => t.user === req.params.user);
+  if (!target) {
+    res.status(404).send("Tweet not found");
+  } else {
+    res.send(target);
   }
 });
 
-// Example of a protected route - run so they can see
-app.get("/protected", authMiddleware, (req, res) => {
-  res.send("User " + req.user + " was authenticated");
+// alternate get request for tweets where user is query param
+// example call: http://localhost:4000/api/tweets?user=Zara
+// app.get('/api/tweets', (req, res) => {
+//     const user = req.query.user
+//     console.log(user)
+//     if (user) {
+//         var target = tweets.find(t => t.user === user)
+//         if (!target) {
+//             res.status(404).send("Tweet not found")
+//         } else {
+//             res.send(target)
+//         }
+//     } else {
+//         res.send(tweets)
+//     }
+// })
+
+// post a tweet
+app.post(
+  "/api/tweets",
+  validateInput,
+  validateTweetLength,
+  async (req, res) => {
+    var tweet = {
+      id: randomInt(200),
+      user: req.body.user,
+      tweet: req.body.tweet,
+    };
+    const tweetsRef = db.collection("tweets").doc("tweets");
+    const snapshot = await tweetsRef.get();
+    const currTweets = snapshot.data().tweets;
+    currTweets.push(tweet);
+    await tweetsRef.update({ tweets: currTweets });
+    res.send(tweet);
+  }
+);
+
+// delete a tweet
+app.delete("/api/tweets", async (req, res) => {
+  const tweetsRef = db.collection("tweets").doc("tweets");
+  const snapshot = await tweetsRef.get();
+  const currTweets = snapshot.data().tweets;
+  const tweetIndex = currTweets.findIndex((tweet) => tweet.id === req.body.id);
+  if (tweetIndex === -1) {
+    res.status(404).send("Tweet not found");
+  } else {
+    // Remove the tweet from the 'tweets' array
+    var removed = currTweets[tweetIndex];
+    console.log(removed);
+    currTweets.splice(tweetIndex, 1);
+    await tweetsRef.update({ tweets: currTweets });
+    res.json(removed);
+  }
 });
+
+// STEP THREE: Create a register endpoint
+
+
+// STEP FOUR: Create a login endpoint. Verifies password + creates token
+
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Listening on port ${port}`))
+app.listen(port, () => console.log(`Listening on port ${port}`));
